11eaa3055d8e712c7f409ddeecfa2f80ffcf7a23
MC-Nation <herojhjh@outlook.com>
2017-06-27 15:06:55 +0700

    Update Main.php

--- src/herojhjh/Main.php
+++ src/herojhjh/Main.php
@@ -1,1 +1,21 @@
 <?php
+
+namespace herojhjh;
+
+use pocketmine\Player;
+use pocketmine\event\{Listener, PlayerGameModeChangeEvent};
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener{
+  public function OnEnable(){
+    $this->getServer()->getLogger()->info(C::GREEN."ClearInventory Enabled.");
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+  public function onDisable(){
+    $this->getLogger()->info(C::RED . "ClearInventory disabled!");
+  }
+  public function OnGameModeChange(PlayerGameModeChangeEvent $event){
+    $player = $event->getPlayer();
+    $player->getInventory()->clearAll();
+  }
+}
